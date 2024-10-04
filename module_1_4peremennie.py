388b3c2c344a93ac80696c2443fe0c4b98c0fb14
Алексей <alex1996-07@yandex.ru>
2024-10-04 20:38:04 +0800

    04/10

--- module_1_4peremennie.py
+++ module_1_4peremennie.py
@@ -1,1 +1,5 @@
-name = 'alex'
+kolvods = 12
+kolvochas = 1.5
+kurs = 'Python'
+kontrol = kolvochas/kolvods
+print(kurs,',', 'всего задач:', kolvods,'затрачено часов:',kolvochas,',','среднее время выполнения', kontrol, 'часа')
