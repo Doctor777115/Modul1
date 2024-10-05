cb7bc15812d91df28520dc8b4daad3de9a3652f9
Алексей <alex1996-07@yandex.ru>
2024-10-05 20:47:55 +0800

    05/10

---
+++ modul1_jrganization_strok.py
@@ -0,0 +1,6 @@
+name = input('Как Вас зовут: ')
+current_year = 2024
+dste_of_burth = input('Вкаком году вы родились? ')
+age = current_year - int(dste_of_burth)
+print('Здравствуйте,', name)
+print ('В этом году Вам', age, 'лет')
